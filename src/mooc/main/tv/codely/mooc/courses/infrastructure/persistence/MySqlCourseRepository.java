4e5c0581df7cfcf2032aa5afebe17ddc1d2cc379
Tavo5691 <gustavobolivar5691@gmail.com>
2020-07-21 00:56:37 -0300

    Added MySqlCourseRepository

--- src/mooc/main/tv/codely/mooc/courses/infrastructure/persistence/MySqlCourseRepository.java
+++ src/mooc/main/tv/codely/mooc/courses/infrastructure/persistence/MySqlCourseRepository.java
@@ -1,22 +1,26 @@
 package tv.codely.mooc.courses.infrastructure.persistence;
 
+import org.hibernate.SessionFactory;
 import tv.codely.mooc.courses.domain.Course;
 import tv.codely.mooc.courses.domain.CourseId;
 import tv.codely.mooc.courses.domain.CourseRepository;
 
-import java.util.HashMap;
 import java.util.Optional;
 
-public final class InMemoryCourseRepository implements CourseRepository {
-    private HashMap<String, Course> courses = new HashMap<>();
+public final class MySqlCourseRepository implements CourseRepository {
+    private SessionFactory sessionFactory;
+
+    public MySqlCourseRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public void save(Course course) {
-        courses.put(course.id().value(), course);
+        sessionFactory.getCurrentSession().save(course);
     }
 
     @Override
     public Optional<Course> search(CourseId id) {
-        return Optional.ofNullable(courses.get(id.value()));
+        return Optional.empty();
     }
 }
